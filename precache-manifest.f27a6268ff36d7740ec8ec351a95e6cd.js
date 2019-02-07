ae6a4815657cb7d6b2b22ac017689a8958d7a9e3
Roman Shauk <shauk60@gmail.com>
2019-02-07 12:38:08 +0200

    Updates

---
+++ precache-manifest.f27a6268ff36d7740ec8ec351a95e6cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dab80533524147f66ed6",
+    "url": "/React-ToDo/static/js/runtime~main.dab80533.js"
+  },
+  {
+    "revision": "2bf75cece12537e3a99d",
+    "url": "/React-ToDo/static/js/main.2bf75cec.chunk.js"
+  },
+  {
+    "revision": "498113eb66a04a4437aa",
+    "url": "/React-ToDo/static/js/1.498113eb.chunk.js"
+  },
+  {
+    "revision": "2bf75cece12537e3a99d",
+    "url": "/React-ToDo/static/css/main.0c150102.chunk.css"
+  },
+  {
+    "revision": "2ebf8fd89af2abb98d29943ca038e09f",
+    "url": "/React-ToDo/index.html"
+  }
+];
